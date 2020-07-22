10c6dc7292881fdab7443d42d7db9643b314f27e
huoxudong125 <423huo@163.com>
2020-07-22 10:07:29 +0800

    Fix  测试请求

--- rest-perf-tester/src/main/java/org/hqf/architect/demo/perf/RestTester.java
+++ rest-perf-tester/src/main/java/org/hqf/architect/demo/perf/RestTester.java
@@ -1,25 +1,70 @@
 package org.hqf.architect.demo.perf;
 
+import org.apache.commons.lang.time.StopWatch;
+
+import javax.lang.model.SourceVersion;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
 
 public class RestTester {
 
-    static CountDownLatch countDownLatch = new CountDownLatch(100);
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 100; i++) {
-            Thread thread = new Thread(() -> {
-                doWork();
-                countDownLatch.countDown();
-            });
-            thread.start();
 
+        if (args.length < 3) {
+            System.out.println("params is: url,concurrentNum, totolReuqest,eg. www.baiud.com,10,100");
         }
-        countDownLatch.await();
-        System.out.println("处理完整个测试的结束时间" + System.currentTimeMillis());
+
+        String url = args[0];
+        int concurrentNum = Integer.parseInt(args[1]);
+        int RequestTestCount = Integer.parseInt(args[2]);
+
+        List<Long> durationSet = Collections.synchronizedList(new ArrayList<>());
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < RequestTestCount; i++) {
+            CountDownLatch countDownLatch = new CountDownLatch(concurrentNum);
+            for (int j = 0; j < concurrentNum; j++) {
+                Thread thread = new Thread(() -> {
+                    long workTime = doWork(url);
+                    synchronized (RestTester.class) {
+                        durationSet.add(workTime);
+                    }
+                    countDownLatch.countDown();
+                });
+                thread.start();
+            }
+            countDownLatch.await();
+        }
+
+
+        stopWatch.stop();
+        System.out.println(String.format("处理完整个测试的结束时间 %d ms,%d", stopWatch.getTime(), durationSet.size()));
+
+        List<Long> DurationSetP95 = new ArrayList<>();
+        Iterator<Long> iterator = durationSet.stream().sorted().iterator();
+        for (int i = 0; i < Math.ceil(durationSet.size() * 0.95); i++) {
+            DurationSetP95.add(iterator.next());
+        }
+        DoubleSummaryStatistics doubleSummaryStatistics = DurationSetP95.stream().mapToDouble(t -> t.doubleValue()).summaryStatistics();
+
+        System.out.println("95% 的输出 avg = " + doubleSummaryStatistics);
     }
 
-    public static void doWork() {
-        System.out.println("处理行数据,时间" + System.currentTimeMillis());
+    public static long doWork(String url) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        try {
+            HttpClientUtil.doGet2(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        stopWatch.stop();
+        long time = stopWatch.getTime();
+        System.out.println(String.format("%s 处理行数据,时间 %d ms", Thread.currentThread().getName(), time));
+        return time;
     }
 }
